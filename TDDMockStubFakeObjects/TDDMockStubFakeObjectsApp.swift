9ce5a0412cfc5aaa61f585824703ed4171362ecb
Austin Dobberfuhl <AUSTIN.DOBBERFUHL4597@STU.MTEC.EDU>
2024-01-31 08:12:31 -0700

    Initial Commit

---
+++ TDDMockStubFakeObjects/TDDMockStubFakeObjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDDMockStubFakeObjectsApp.swift
+//  TDDMockStubFakeObjects
+//
+//  Created by Austin Dobberfuhl on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct TDDMockStubFakeObjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
