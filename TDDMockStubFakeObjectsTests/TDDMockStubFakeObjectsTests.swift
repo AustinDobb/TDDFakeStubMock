8baf6b22df1512c4ef0801221e0bf3faa029ac10
Austin Dobberfuhl <AUSTIN.DOBBERFUHL4597@STU.MTEC.EDU>
2024-02-02 10:02:36 -0700

    Completed git commit -m completed
    help

--- TDDMockStubFakeObjectsTests/TDDMockStubFakeObjectsTests.swift
+++ TDDMockStubFakeObjectsTests/TDDMockStubFakeObjectsTests.swift
@@ -25,12 +25,93 @@ final class TDDMockStubFakeObjectsTests: XCTestCase {
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
+}
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+protocol ArcadeGameManager {
+    func fetchAllGames(completed: (Data?) -> Void)
+}
+
+//Fake Object
+class FakeGamesLibrary: ArcadeGameManager {
+    func fetchAllGames(completed: (Data?) -> Void) {
+        completed(nil)
+    }
+}
+
+class StubGameLibrary: ArcadeGameManager {
+    let data: Data
+    
+    func fetchAllGames(completed: (Data?) -> Void) {
+        let data = Data()
+        completed(data)
+    }
+    
+    init(data: Data) {
+        self.data = data
+    }
+}
+
+class MockGameLibrary: ArcadeGameManager {
+    var fetchAllGamesCalled = false
+    
+    func fetchAllGames(completed: (Data?) -> Void) {
+        fetchAllGamesCalled = true
+    }
+    
+    
+}
+
+class ArcadeGames {
+    private let arcadeGameManager: ArcadeGameManager
+    
+    init(arcadeGameManager: ArcadeGameManager) {
+        self.arcadeGameManager = arcadeGameManager
+    }
+    
+    func doingWork(completed: (Data?) -> Void) {
+        arcadeGameManager.fetchAllGames { data in
+            completed(data)
         }
     }
-
+    
+    class ArcadeGamesTest: XCTestCase {
+        
+        func testFakeGameLibrary() {
+            
+            let fakeGameLibrary = FakeGamesLibrary()
+            let arcadeGames = ArcadeGames(arcadeGameManager: fakeGameLibrary)
+            var completionData: Data?
+            
+            arcadeGames.doingWork { data in
+                completionData = data
+            }
+            
+            XCTAssertNil(completionData)
+            
+        }
+        
+        func testStubGameLibrary() {
+            let data = Data()
+            let stubGameLibrary = StubGameLibrary(data: data)
+            let arcadeGames = ArcadeGames(arcadeGameManager: stubGameLibrary)
+            var completionData: Data?
+            
+            arcadeGames.doingWork { data in
+                completionData = data
+            }
+            
+            XCTAssertEqual(completionData, data)
+            
+        }
+        
+        func testMockGameLibrary() {
+            let mockGameLibrary = MockGameLibrary()
+            let arcadeGames = ArcadeGames(arcadeGameManager: mockGameLibrary)
+            
+            arcadeGames.doingWork { _ in }
+            
+            XCTAssertTrue(mockGameLibrary.fetchAllGamesCalled)
+        }
+        
+    }
 }
